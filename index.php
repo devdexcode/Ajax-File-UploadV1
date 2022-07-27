3073bec6475c40add34591771d63c8db0d7ff264
= <=>
2022-07-27 06:54:12 +0500

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+/*
+Plugin Name: AJAX File Upload
+Plugin URI: 
+Description: Can add ajax file uploader that returns the file url inside a form.
+Author:  Aamir Hussain
+Version: 0.1
+Author URI: 
+*/
