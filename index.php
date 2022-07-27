a6659775a40fc1a7b8baf7066e174fa5350fe30f
= <=>
2022-07-27 07:56:27 +0500

    version 1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: AJAX File Upload
 Plugin URI: 
-Description: Can add ajax file uploader that returns the file url inside a form.
+Description: Can add ajax file uploader that returns the file url inside a form. just add inside the script tag in the footer:  <br>add_file_upload_to_form( <br>add_after, //to add after what? <br> container_css_class, //the file upload parent div<br>upload_class, // apply class to input file upload<br>image_preview_class, // apply class to preview img tag parent<br>preview_image_class// apply class to preview img tag  );  
 Author:  Aamir Hussain
 Version: 0.1
 Author URI: 
@@ -15,10 +15,7 @@ function aamirs_footer_script(){
     ob_start();?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
- 
- $(document).ready(function(){
- //allowed_file_types, max_file_size 
- function add_file_upload_to_form(add_after, container_css_class, upload_class, image_preview_class, preview_image_class){
+  function add_file_upload_to_form(add_after, container_css_class, upload_class, image_preview_class, preview_image_class){
      let the_html = '<div class="upload_container '+container_css_class+'"><input accept="image/*" type="file" class="file '+upload_class+'" name="file" /><input type="hidden" class="file_src" name="file_src" /><div class="preview '+image_preview_class+'"><img style="display:none;" src="" class="preview_img '+preview_image_class+'" width="100" height="100"></div><div class="response col"></div></div>';
      $(the_html).insertAfter(add_after);
  
@@ -59,6 +56,9 @@ function aamirs_footer_script(){
          }
          });
  }
+ $(document).ready(function(){
+ //allowed_file_types, max_file_size 
+
  /* function add_file_upload_to_form(
       add_after, //to add after what?
       container_css_class, //the file upload parent div
@@ -66,26 +66,10 @@ function aamirs_footer_script(){
       image_preview_class, // apply class to preview img tag parent
       preview_image_class// apply class to preview img tag 
  );*/       
- add_file_upload_to_form('p.mt-4.mb-4:nth-of-type(2)', 'row p4', 'col', 'col', 'image-fluid');
+
  });
- 
+
   </script>  
-
-
-<script>
-(function ($) {
-$(document).ready(function() {	
-/* add_file_upload_to_form(
-	add_after, //to add after what?
-	container_css_class, //the file upload parent div
-	upload_class, // apply class to input file upload
-	image_preview_class, // apply class to preview img tag parent
-	preview_image_class// apply class to preview img tag 
-);*/
-
-});
-})(jQuery);	
-</script> 
     <?php $html = ob_get_clean();
     echo $html;
 }
@@ -94,4 +78,4 @@ add_action('wp_ajax_the_upload_process', 'the_upload_process');
 add_action('wp_ajax_nopriv_the_upload_process', 'the_upload_process');
 function the_upload_process(){
     include_once('upload.php');
-  }
+}
